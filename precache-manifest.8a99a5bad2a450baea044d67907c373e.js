cde1ad27d80fafe3f036c3b5aacdd7b9414c004a
Mehdi Messarat <medimessarat@gmail.com>
2020-09-20 16:25:41 +0100

    Create our first statis

---
+++ precache-manifest.8a99a5bad2a450baea044d67907c373e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80a9ddd5b77d599c4288ff7d7c099a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840b6ac7079194747654",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36c1d3ccdd174508d599",
+    "url": "/static/js/2.cd6ef5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd6ef5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840b6ac7079194747654",
+    "url": "/static/js/main.841d4e1f.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
